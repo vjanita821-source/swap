6fb1ad25bb5b440ab93b0e05df4c9af2861221f8
janita <vjanita821@gmail.com>
2026-09-15 09:41:19 -0700

    Updated the  swap

--- swap.py
+++ swap.py
@@ -1,11 +1,14 @@
+print("Welcome to Swap Program")
+
 a = 10
 b = 20
 
-
+print("Before swapping:")
+print("a =", a, "b =", b)
 
 temp = a
 a = b
 b = temp
 
-
+print("After swapping:")
 print("a =", a, "b =", b)
